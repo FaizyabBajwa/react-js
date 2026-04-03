755a5b89625ecfc7a642b95b66f8aa24e9751589
FaizyabBajwa <faizyabbajwa18@gmail.com>
2026-04-04 00:46:50 +0500

    add first-react files and custom react folder

---
+++ first-react/src/App.jsx
@@ -0,0 +1,16 @@
+
+
+import Chai from './chai.jsx'
+
+
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Hello from React with vite!</h1>
+    <p>This is a simple paragraph.</p>
+    </>
+    
+  )
+}
+export default App
